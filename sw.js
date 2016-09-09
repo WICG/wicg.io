05886c6dc5782f3b60b946f85dfe384c59898c11
Marcos Caceres <marcos@marcosc.com>
2016-09-09 13:30:40 +1000

    feat(offline): put user in charge of update

--- sw.js
+++ sw.js
@@ -1,82 +1,66 @@
-/*globals async, caches, siteFiles, Response, fetch */
+/*globals self, async, caches, fetch */
 "use strict";
+self.importScripts("/js/lib/async.js");
 
-// Let's start bringing in things we need
-importScripts("js/lib/async.js");
-importScripts("js/siteFiles.js");
+const SITE_CACHE = "site-v1";
 
-// Set up Service Worker tasks
-const SWTasks = {
-  deleteAllCaches: async(function*() {
+self.addEventListener("install", (ev) => {
+  const resources = [
+    "/",
+    "/images/code@1x.jpg",
+    "/images/document.svg",
+    "/images/github.svg",
+    "/images/irc.svg",
+    "/images/link.svg",
+    "/images/logo.svg",
+    "/images/twitter_white.svg",
+    "/images/w3c_white.svg",
+    "/js/accordion.js",
+    "/js/lib/async.js",
+    "/manifest.json",
+    "/styles/fonts/nexa/Nexa_Bold.otf",
+    "/styles/style.css",
+  ];
+  ev.waitUntil(async.task(function*() {
+    const cache = yield caches.open(SITE_CACHE);
+    yield cache.addAll(resources);
+  }));
+});
+
+self.addEventListener("activate", () => {
+  async.task(function*() {
     const keys = yield caches.keys();
-    const promisesToDelete = keys.map(
-      key => caches.delete(key)
-    );
-    yield Promise.all(promisesToDelete);
-  }),
-  cacheSite: async(function*(siteFiles) {
-    console.log("Trying to offline site");
-    try {
-      const cache = yield caches.open("site_cache");
-      yield cache.addAll(siteFiles);
-    } catch (err) {
-      console.error(err);
-    }
-    console.log("Done caching site files!", siteFiles.join("\n"));
-  }),
-  respondFromCache: async(function*(request, cacheName) {
-    const cache = yield caches.open(cacheName);
-    const response = yield cache.match(request);
-    // We have this, yay!
+    yield keys
+      .filter(key => key !== SITE_CACHE)
+      .map(key => caches.delete(key));
+  });
+});
+
+self.addEventListener("message", ({ data: { action } }) => {
+  switch (action) {
+    case "skipWaiting":
+      self.skipWaiting();
+      break;
+  }
+});
+
+self.addEventListener("fetch", (ev) => {
+  ev.respondWith(async.task(function*() {
+    const response = yield caches.match(ev.request);
     if (response) {
       return response;
     }
-    // Try to get it from the network and store it
+    console.warn("No caches match for?:", ev.request.url);
+    // go to network instead
     try {
-      console.warn("Not in cache. Getting from network:", request.url || request);
-      response = yield fetch(request);
-      yield cache.put(request, response.clone());
-    } catch (err) {
-      var msg = `failed to store ${request.url || request} in ${cacheName}.`;
-      console.warn(msg, err);
-      // ensure a response
-      response = new Response();
-    }
-    return response;
-  }),
-  saveToCache: async(function*(request, response, cacheName) {
-    const cache = yield caches.open(cacheName);
-    yield cache.put(request, response.clone());
-  }),
-};
-
-// Let's set up life cycle listeners ("install", "activate", "fetch")
-self.addEventListener("install", async(function*() {
-  yield SWTasks.deleteAllCaches();
-  yield SWTasks.cacheSite(siteFiles);
-  console.log("Successfully completed install tasks...");
-}));
-
-self.addEventListener("fetch", function(ev) {
-  console.info("Handling", ev.request.url, "going to network first...");
-  const networkFirst = async(function*() {
-    let response;
-    try {
-      response = yield fetch(ev.request);
-      if (!response.ok) {
-        throw new Error("Got response, but was not ok!", response.status);
+      const netResponse = yield fetch(ev.request);
+      if (netResponse.ok) {
+        return netResponse;
       }
     } catch (err) {
-      console.error("Useless response, going to cache:", err);
-      return yield SWTasks.respondFromCache(ev.request, "site_cache");
+      // just return the index if all goes bad.
+      console.error(err);
     }
-    // Response was ok, so let's cache it!
-    try {
-      yield SWTasks.saveToCache(ev.request, response, "site_cache");
-    } catch (err) {
-      console.error("Oh crap! Wasn't able to write to cache!", err);
-    }
-    return response;
-  });
-  ev.respondWith(networkFirst());
+    return yield caches.match("/");
+  }));
 });
