e6a43a2ae4eca4c6a5a7d7b7c220f80ee1e65ba9
Marcos Cáceres <marcos@marcosc.com>
2023-02-28 14:01:16 -0800

    bug fixes and improvements (#23)

--- generator.js
+++ generator.js
@@ -6,65 +6,101 @@ const gh = require("simple-github")({
   token: `${process.env.WICG_TOKEN}`,
 });
 
-const ms_to_days_ratio = 1000*60*60*24;
+const ms_to_days_ratio = 1000 * 60 * 60 * 24;
 
-const issue_score = activity => {
-    let score = 0;
-    const date = new Date();
-    for (issue of activity) {
-        const issue_update_date = new Date(issue.updated_at);
+const issue_score = (activity) => {
+  let score = 0;
+  const date = new Date();
+  for (issue of activity) {
+    const issue_update_date = new Date(issue.updated_at);
 
-        const weeks_ago = Math.floor((date - issue_update_date) / (ms_to_days_ratio * 7));
-        const weight = Math.max(1, 52 - weeks_ago);
-        score += weight;
-    }
-    return score;
+    const weeks_ago = Math.floor(
+      (date - issue_update_date) / (ms_to_days_ratio * 7)
+    );
+    const weight = Math.max(1, 52 - weeks_ago);
+    score += weight;
+  }
+  return score;
 };
-const commit_score = activity => {
-    if (!activity.isPrototypeOf("Array"))
-        return 0;
-    let score = 0;
-    let weight = 1;
-    for (week of activity) {
-        score += weight * week.total;
-        ++weight;
-    }
-    return score;
+const commit_score = (activity) => {
+  if (!activity.isPrototypeOf("Array")) return 0;
+  let score = 0;
+  let weight = 1;
+  for (week of activity) {
+    score += weight * week.total;
+    ++weight;
+  }
+  return score;
 };
 const combined_score = (issue, commit) => {
-    return issue * 0.7 + commit * 0.3;
-}
+  return Math.floor(issue * 0.7 + commit * 0.3);
+};
 
-const ignore_set = {"admin": true, "wicg.io": true, "starter-kit": true, "repo_info_generator": true};
+const ignore_set = new Set([
+  "admin",
+  "wicg.io",
+  "starter-kit",
+  "repo_info_generator",
+  "discourse-wicg-theme",
+  "proposals",
+  "wicg.github.io",
+]);
 
 (async () => {
-    let active = [];
-    let archived = []
-    const repos = await gh.request("GET /orgs/:owner/repos", {});
-    for (repo of repos) {
-        if (ignore_set[repo.name]) {
-            continue;
-        }
-
-        let repo_object = {};
-        repo_object["name"] = repo.name;
-        repo_object["repo"] = repo.html_url;
-        repo_object["spec"] = repo.homepage;
-        if (repo.archived) {
-             archived.push(repo_object);
-             continue;
-        }
-        const commit_activity = await gh.request("GET /repos/:owner/" + repo.name + "/stats/commit_activity", {});
-        const issues = await gh.request("GET /repos/:owner/" + repo.name + "/issues?state=all", {});
-        repo_object["score"] = combined_score(commit_score(commit_activity), issue_score(issues));
-        active.push(repo_object);
+  let active = [];
+  let archived = [];
+  const repos = await gh.request("GET /orgs/:owner/repos", {});
+  for (repo of repos.filter((filter) => !ignore_set.has(filter.name))) {
+    const repo_object = {
+      name: repo?.name ?? null,
+      repo: repo?.html_url ?? null,
+      spec: repo?.homepage ?? null,
+      stars: repo?.stargazers_count ?? null,
+      description: repo?.description ?? null,
+      has_pages: repo?.has_pages ?? null,
+      created_at: repo?.created_at ?? null,
+      updated_at: repo?.updated_at ?? null,
+      // A proxy for last activity and when it was archived
+      pushed_at: repo?.pushed_at ?? null,
+    };
+    if (repo.archived) {
+      archived.push(repo_object);
+      continue;
     }
-// sort the damn thing
-active = active.sort((a,b) => { return b.score - a.score; });
-try {
-    await fs.writeFile(path.resolve(__dirname, "repos.json"), JSON.stringify(active, null, 2), "utf-8");
-} catch (err) {
+    const commit_activity = await gh.request(
+      "GET /repos/:owner/" + repo.name + "/stats/commit_activity",
+      {}
+    );
+    const issues = await gh.request(
+      "GET /repos/:owner/" + repo.name + "/issues?state=all",
+      {}
+    );
+    repo_object["score"] = combined_score(
+      commit_score(commit_activity),
+      issue_score(issues)
+    );
+    active.push(repo_object);
+  }
+  // sort the damn thing
+  active = active.sort((a, b) => {
+    return b.score - a.score;
+  });
+  // Sort by pushed_at Date
+  archived = archived.sort((a, b) => {
+    return new Date(b.pushed_at) - new Date(a.pushed_at);
+  });
+  try {
+    await fs.writeFile(
+      path.resolve(__dirname, "./data/active.json"),
+      JSON.stringify(active),
+      "utf-8"
+    );
+    await fs.writeFile(
+      path.resolve(__dirname, "./data/archived.json"),
+      JSON.stringify(archived),
+      "utf-8"
+    );
+  } catch (err) {
     console.error(err);
-}
-})()
-
+  }
+})();
